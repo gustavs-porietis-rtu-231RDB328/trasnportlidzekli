94b79e59c4c142077fb9fd1976b50acc561eb5d5
Gustavs Porietis 231RDB328 <144696225+gustavs-porietis-rtu-231RDB328@users.noreply.github.com>
2025-05-13 19:05:05 +0000

    2

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import requests
 import json
+import googlemaps
+from geopy.distance import geodesic
 
 url = "https://api.vuumly.com/api/availableDevices"
 availableVehiclesJson = requests.get(url, stream = False)
 availableVehicles = json.loads(availableVehiclesJson.text)
 
-#for vehicle in availableVehicles:
-    #print(vehicle["distinct_place"])
-
 origin = input("Ievadiet sākuma lokāciju: ")
 destination = input("Ievadiet galamērķi: ")
 
+gmaps = googlemaps.Client(key="AIzaSyBHcAg-O9jXgFnyLLwYwN-j4sSLavKsRas")
+geocode_origin = gmaps.geocode(origin)
+originCoordinates = geocode_origin[0]["geometry"]["location"]
+originCoordinates = (originCoordinates["lat"], originCoordinates["lng"])
+
+for vehicle in availableVehicles[:]:
+    vehicleLoaction = (vehicle["location"]["lat"], vehicle["location"]["lng"])
+    geodesic(vehicleLoaction, originCoordinates).km
+    vehicle["distance"] = geodesic(vehicleLoaction, originCoordinates).km
+    if vehicle["distance"] > 1.0:
+        availableVehicles.remove(vehicle)
+
+print(availableVehicles)
