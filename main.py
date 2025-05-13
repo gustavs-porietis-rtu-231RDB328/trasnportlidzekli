0ae341f44e1be7ec97f08943796d301f6ec481c5
Gustavs Porietis 231RDB328 <144696225+gustavs-porietis-rtu-231RDB328@users.noreply.github.com>
2025-05-13 19:53:58 +0000

    3

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from geopy.distance import geodesic
 url = "https://api.vuumly.com/api/availableDevices"
 availableVehiclesJson = requests.get(url, stream = False)
 availableVehicles = json.loads(availableVehiclesJson.text)
+availableCars = []
+availableScooters = []
 
 origin = input("Ievadiet sākuma lokāciju: ")
 destination = input("Ievadiet galamērķi: ")
@@ -21,5 +23,17 @@ for vehicle in availableVehicles[:]:
     vehicle["distance"] = geodesic(vehicleLoaction, originCoordinates).km
     if vehicle["distance"] > 1.0:
         availableVehicles.remove(vehicle)
+    else:
+        directionsToVehicle = gmaps.directions(origin, vehicleLoaction, mode = "walking")
+        vehicle["walk_time"] = directionsToVehicle[0]["legs"][0]["duration"]["value"]
+        if vehicle["device_type"] == "car":
+            directionsToDestination = gmaps.directions(vehicleLoaction, destination, mode = "driving")
+            vehicle["drive_duration"] = directionsToDestination[0]["legs"][0]["duration"]["value"]
+            availableCars.append(vehicle)
+        elif vehicle["device_type"] == "scooter":
+            directionsToDestination = gmaps.directions(vehicleLoaction, destination, mode = "walking")
+            vehicle["ride_duration"] = directionsToDestination[0]["legs"][0]["duration"]["value"]
+            availableScooters.append(vehicle)
 
-print(availableVehicles)
+#print(availableScooters)
+#print(gmaps.directions(origin, destination, mode = "driving")[0]["legs"][0]["duration"]["value"])
